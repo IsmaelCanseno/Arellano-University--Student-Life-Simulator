9309d8fa75745f307d60829788ac3b8dfe2f2d2a
thfrndx <t.fernando0323@gmail.com>
2025-03-25 20:38:36 +0800

    update

--- arellano-univesity--student-life-simulator/Scripts/house.gd
+++ arellano-univesity--student-life-simulator/Scripts/house.gd
@@ -30,6 +30,6 @@ func _ready():
 	playerNode.global_position = marker.global_position
 		
 func _on_level_spawn(destination_tag: String):
-		var door_path = "Door_" + destination_tag
-		var door = get_node(door_path) as Door
-		NavigationManager.trigger_player_spawn(door.spawn.global_position, door.spawn_direction)
+	var door_path = "Door_" + destination_tag
+	var door = get_node(door_path) as Door
+	NavigationManager.trigger_player_spawn(door.spawn.global_position, door.spawn_direction)
